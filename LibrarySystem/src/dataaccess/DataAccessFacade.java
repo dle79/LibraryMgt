bfa48030dd2bd0350216f3a01ec350331ae11071
nnkhuong <nguyenngockhuong@gmail.com>
2015-07-29 08:29:20 +0700

    detect OS machine to get actually path of file

--- LibrarySystem/src/dataaccess/DataAccessFacade.java
+++ LibrarySystem/src/dataaccess/DataAccessFacade.java
@@ -21,7 +21,9 @@ public class DataAccessFacade implements DataAccess {
 	}
 
 	public static final String OUTPUT_DIR = System.getProperty("user.dir")
-			+ "\\src\\dataaccess\\storage";
+			+ ((System.getProperty("os.name").startsWith("Windows"))? "\\src\\dataaccess\\storage" : "/src/dataaccess/storage");
+	
+	
 	public static final String DATE_PATTERN = "MM/dd/yyyy";
 
 
