1c13c3eaca3344bfcd222470bd4df1749ced3a6b
nnkhuong <nguyenngockhuong@gmail.com>
2015-07-31 20:43:42 +0700

    keep background pic for main form

--- LibrarySystem/src/ui/LibrarySystem.java
+++ LibrarySystem/src/ui/LibrarySystem.java
@@ -1,9 +1,12 @@
 package ui;
 
 
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,8 +16,15 @@ public class LibrarySystem extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		try {
 			VBox root = (VBox)FXMLLoader.load(getClass().getResource("LibrarySystem.fxml"));
+
+			primaryStage.setTitle("MUM Library Management System");
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			File fileIcon = new File(System.getProperty("user.dir") + "/src/ui/library_icon.jpg");
+
+			Image imageIcon = new Image(fileIcon.toURI().toURL().toExternalForm());
+
+			primaryStage.getIcons().add(imageIcon);
+			scene.getStylesheets().add(getClass().getResource("applicationMain.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			primaryStage.setResizable(false);
