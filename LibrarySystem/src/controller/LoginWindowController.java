600865707514502d55f5bada29210a26ce9d1f57
unknown <yxm1014@gmail.com>
2015-07-31 02:57:49 -0500

    add searchAuth method

---
+++ LibrarySystem/src/controller/LoginWindowController.java
@@ -0,0 +1,135 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+import dataaccess.Auth;
+import exception.LoginException;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import ui.LibrarySystem;
+
+public class LoginWindowController implements Initializable {
+
+	private static final Logger log = Logger.getLogger(LoginWindowController.class.getName());
+
+	private SystemController sysController = null;
+
+	private static Auth role;
+
+
+	@FXML
+	private Button resetBtn;
+
+	@FXML
+	private TextField usernameTfd;
+
+	@FXML
+	private PasswordField passwordTfd;
+
+	@FXML
+	private Label promptLabel;
+
+	@FXML
+	private Button loginBtn;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@FXML
+	private void handleloginBtn(MouseEvent event) {
+		try {
+			actionOnLogin();
+		} catch (LoginException e) {
+			log.warning(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void actionOnLogin() throws LoginException, IOException {
+		String username = usernameTfd.getText();
+		String password = passwordTfd.getText();
+
+		log.info("user " + username);
+		log.info("pass " + password);
+
+		sysController = SystemController.getInstance();
+
+		try {
+			sysController.login(username, password);
+		} catch (LoginException e) {
+			promptLabel.setText(e.getMessage());
+			throw e;
+		}
+		promptLabel.setText("");
+		log.info("Successful login!");
+
+		try {
+			Stage stage = (Stage) promptLabel.getScene().getWindow();
+			stage.close();
+			new LibrarySystem().start(new Stage());
+		} catch (Exception e) {
+			log.info(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public Auth getUserAuth() {
+		return role;
+	}
+
+	@FXML
+	private void handleLoginBtnOnKeyReleased(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER) {
+			loginBtn.requestFocus();
+			loginBtn.setEffect(new DropShadow());
+			try {
+				actionOnLogin();
+			} catch (LoginException e) {
+				loginBtn.setEffect(null);
+				log.warning(e.getMessage());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@FXML
+	private void handleResetBtn(MouseEvent event) {
+		actionOnReset();
+	}
+
+	@FXML
+	private void handleResetBtnOnKeyReleased(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER) {
+			actionOnReset();
+		}
+	}
+
+	private void actionOnReset() {
+		usernameTfd.setText("");
+		passwordTfd.setText("");
+		promptLabel.setText("");
+		loginBtn.setEffect(null);
+	}
+
+
+
+}
